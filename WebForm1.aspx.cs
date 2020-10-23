4f261a6425c218e4860654ecc576122739dc7822
senadin007 <senadin.omerovic@empirica.edu.ba>
2020-10-23 21:46:34 +0200

    ODZAJJSUS

--- WebForm1.aspx.cs
+++ WebForm1.aspx.cs
@@ -1,147 +1,30 @@
-﻿using HtmlAgilityPack;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Data;
-using System.Data.Entity;
+﻿using System;
 using System.Data.SqlClient;
 using System.Linq;
-using System.Runtime.Remoting.Metadata.W3cXsd2001;
-using System.Text;
 using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace WebApp
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
-        List<string> Tickers = new List<string>();
-        string Company;
-        SqlConnection cnn;
-        SqlCommand cmd;
-        string con = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\User01\Desktop\Zadatak1\WebApp\App_Data\dbYahoo.mdf;Integrated Security=True";
+        string con = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" + AppDomain.CurrentDomain.BaseDirectory + @"App_Data\dbYahoo.mdf;Integrated Security=True";
 
         protected void Page_Load(object sender, EventArgs e)
         {
             Calendar1.SelectedDate = DateTime.Now;
             lblInfo.Visible = false;
-        }
-
-        void getData(DateTime date)
-        {
-            lblInfo.Visible = true;
-            lblInfo.Text = "Data is saving. Please wait...";
-            string period1 = date.AddDays(-3).ToString("yyyy-MM-dd");
-            string period2 = date.AddDays(+3).ToString("yyyy-MM-dd");
-            string period = date.ToString("yyyy-MM-dd");
-
-            string htmlLink = string.Format("https://finance.yahoo.com/calendar/earnings?from={0}&to={1}&day={2}", period1, period2, period);
-            var getHtmlWeb = new HtmlWeb();
-            var document = getHtmlWeb.Load(htmlLink);
-            var aTags = document.DocumentNode.SelectNodes("//td//a[contains(@class, 'Fw(600) C($linkColor)')]");
-            int counter = 1;
-            if (aTags != null)
-            {
-                foreach (var aTag in aTags)
-                {
-                    string d = aTag.InnerHtml;
-
-                    string textOnly = HttpUtility.HtmlDecode(d.ToString());
-
-                    Tickers.Add(getYahooInfo(period,textOnly));                                     
-                                     
-                    counter++;
-                }
-            }
-            dsYahoo ds = new dsYahoo();
-            SqlDataAdapter adap = new SqlDataAdapter("SELECT * FROM YahooTable", con);
-            adap.Fill(ds, "YahooTable");
-
-            GridView1.DataSource = ds.Tables["YahooTable"].DefaultView; 
-            GridView1.DataBind();
-
-        }
+        }      
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-            Delete_Data();
-            getData(Calendar1.SelectedDate);
+            YahooInfo YahooData = new YahooInfo();
+            YahooData.SaveData(Calendar1.SelectedDate);
+            dsYahoo ds = new dsYahoo();
+            SqlDataAdapter adap = new SqlDataAdapter("SELECT * FROM YahooTable", con);
+            adap.Fill(ds, "YahooTable");
+            GridView1.DataSource = ds.Tables["YahooTable"].DefaultView;
+            GridView1.DataBind(); 
             lblInfo.Visible = false;
-         
-        }
-
-        string getYahooInfo(string period,string symbol)
-        {
-            StringBuilder textString = new StringBuilder();
-            YahooInfo  wc1 = new YahooInfo();
-            Dictionary<DateTime, YahooInfoItems > downloadedStockData;
-
-            //request data from Yahoo for the past 10 days
-            downloadedStockData = wc1.getDataFromYahoo(symbol);
-            GetFullName(symbol);
-
-            foreach (KeyValuePair<DateTime, YahooInfoItems> singleItem in downloadedStockData)
-            {
-                Insert_Data(symbol, Company, singleItem.Value.YearFounded, singleItem.Value.Employees, singleItem.Value.HQCity, singleItem.Value.HQStreet, Calendar1.SelectedDate, singleItem.Value.previousClose, singleItem.Value.open, singleItem.Value.marketCap);
-              
-            }
-           
-            return  textString.ToString();
-        }
-        public void Insert_Data(string _Symbol,string _Company, int _Year, Double _Employees, string _City, string _Street, DateTime _Period, Double _PCP, Double _OP, Double _MC)
-        {
-            
-            String query = "INSERT INTO [dbo].[YahooTable] ([Symbol], [CompanyName], [Year], [Employees], [HqCity], [HqStreet], [Date], [PCP], [OP], [MC]) VALUES (@Symbol, @CompanyName, @Year, @Employees, @HqCity, @HqStreet, @Date, @PCP, @OP, @MC)";
-
-            cnn = new SqlConnection(con);
-            cnn.Open();
-            cmd = new SqlCommand(query, cnn);
-
-            cmd.Parameters.AddWithValue("@Symbol", _Symbol);
-            cmd.Parameters.AddWithValue("@CompanyName", _Company);
-            cmd.Parameters.AddWithValue("@Year", _Year);
-            cmd.Parameters.AddWithValue("@Employees", _Employees);
-            cmd.Parameters.AddWithValue("@HqCity", _City);
-            cmd.Parameters.AddWithValue("@HqStreet", _Street);
-            cmd.Parameters.AddWithValue("@Date", _Period);
-            cmd.Parameters.AddWithValue("@PCP", _PCP);
-            cmd.Parameters.AddWithValue("@OP", _OP);
-            cmd.Parameters.AddWithValue("@MC", _MC);
-            cmd.ExecuteNonQuery();
-            cnn.Close();
-                       
-        }
-
-        public void Delete_Data()
-        {
-                        
-            cnn = new SqlConnection(con);
-            cnn.Open();
-            cmd = new SqlCommand("Truncate table YahooTable", cnn);
-            cmd.ExecuteNonQuery();
-            cnn.Close();
-                     
-         }
-
-        private void GetFullName(string symbol)
-        {
-            
-            string htmlLink = string.Format("https://finance.yahoo.com/quote/{0}/profile?p={0}", symbol);
-            var getHtmlWeb = new HtmlWeb();
-            var document = getHtmlWeb.Load(htmlLink);
-            var aTags = document.DocumentNode.SelectNodes("//h1[contains(@class, 'D(ib) Fz(18px)')]");
-            int counter = 1;
-            if (aTags != null)
-            {
-                foreach (var aTag in aTags)
-                {
-                    string d = aTag.InnerHtml;
-                    Company  = HttpUtility.HtmlDecode(d.ToString());
-                    counter++;
-                }
-            }
-
         }
 
     }
--- YahooInfo.cs
+++ YahooInfo.cs
@@ -5,24 +5,44 @@ using System.Linq;
 using System.Web;
 using System.Net;
 using System.Web.ModelBinding;
+using System.Data.SqlClient;
+using Microsoft.VisualBasic.FileIO;
+using CsvHelper;
+using System.Web.UI;
 
 namespace WebApp
 {
     public class YahooInfo
                
     {
+        public string Symbol = "";
+        public string CompanyName = "";
+        public int YearFounded = 0;
+        public double Employees = 0;
+        public string HQCity = "";
+        public string HQStreet = "";
+        public double previousClose = 0;
+        public double open = 0;
+        public double marketCap = 0;
+
+        SqlConnection cnn;
+        SqlCommand cmd;
+        string con = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" + AppDomain.CurrentDomain.BaseDirectory + @"App_Data\dbYahoo.mdf;Integrated Security=True";
+
         WebClient webConnector;
 
         const string yahooAddressP = "https://query1.finance.yahoo.com/v10/finance/quoteSummary/[-|ticker|-]?modules=assetProfile";
         const string yahooAddressS = "https://query1.finance.yahoo.com/v10/finance/quoteSummary/[-|ticker|-]?modules=summaryDetail";
+        const string historyAddress = "https://query1.finance.yahoo.com/v7/finance/download/[-|ticker|-]?period1=[-|period1|-]&period2=[-|period2|-]&interval=1d";
 
-      
-        public Dictionary<DateTime, YahooInfoItems> getDataFromYahoo(string ticker)
+        public void SaveData(DateTime Period)
         {
-          return  fillDataDictionary(getData(constructYahooLink(ticker, yahooAddressP),true), getData(constructYahooLink(ticker, yahooAddressS), false));
-         }
+            Delete_Data();
+            GetDataFromCSV(Period);
+        }
+             
                         
-        string constructYahooLink(string ticker,string yahooAddress)
+        string ConstructYahooLink(string ticker,string yahooAddress)
         {
             string constructedUri = yahooAddress;
 
@@ -30,14 +50,25 @@ namespace WebApp
 
             return constructedUri;
         }
-               
-        string getData(string webpageUriString, Boolean profile)
+
+        string ConstructYahooHistoryLink(DateTime period1, DateTime period2, string ticker, string yahooAddress)
+        {
+            string constructedUri = yahooAddress;
+            long unixTime1 = ((DateTimeOffset)period1).ToUnixTimeSeconds();
+            long unixTime2 = ((DateTimeOffset)period2).ToUnixTimeSeconds();
+
+            constructedUri = constructedUri.Replace("[-|ticker|-]", ticker);
+            constructedUri = constructedUri.Replace("[-|period1|-]", unixTime1.ToString());
+            constructedUri = constructedUri.Replace("[-|period2|-]", unixTime2.ToString());
+
+            return constructedUri;
+        }
+
+        string GetData(string webpageUriString, int type)
         {
             string tempStorageString = "";
             try
             {
-
-
                 if (webpageUriString != "")
                 {
                     using (webConnector = new WebClient())
@@ -46,15 +77,16 @@ namespace WebApp
                         {
                             using (StreamReader responseStreamReader = new StreamReader(responseStream))
                             {
-                                //extract the response we got from the internet server
                                 tempStorageString = responseStreamReader.ReadToEnd();
 
-                                //change the unix style line endings so they work here
-                                if (profile == false)
+                                if (type == 1)
                                 {
                                     tempStorageString = tempStorageString.Replace("\n", Environment.NewLine);
                                 }
-
+                                else if (type == 2)
+                                {
+                                    tempStorageString = tempStorageString.Replace("\n", ",");
+                                }
                             }
                         }
                     }
@@ -66,12 +98,28 @@ namespace WebApp
             }
             return tempStorageString;
             }
-             
-        Dictionary<DateTime, YahooInfoItems > fillDataDictionary(string csvDataP, string csvDataS)
-        {
-            Dictionary<DateTime, YahooInfoItems> parsedStockData = new Dictionary<DateTime, YahooInfoItems>();
 
-            YahooInfoItems newItem = new YahooInfoItems();
+        public void GetDataFromCSV(DateTime Period)
+        {
+            using (TextFieldParser csvReader = new TextFieldParser(AppDomain.CurrentDomain.BaseDirectory + @"App_Data\tickers1.csv"))
+            {
+                csvReader.SetDelimiters(new string[] { "," });
+                csvReader.HasFieldsEnclosedInQuotes = true;
+                string[] Fields = csvReader.ReadFields();
+               
+                foreach (string ticker in Fields)
+                {
+                    Symbol = ticker;
+                    FillData(GetData(ConstructYahooLink(ticker, yahooAddressP), 0), GetData(ConstructYahooLink(ticker, yahooAddressS), 1), GetData(ConstructYahooHistoryLink(Period, Period.AddDays(1), ticker, historyAddress), 2));
+                    GetFullName(Symbol);
+                    Insert_Data(Symbol, CompanyName, YearFounded, Employees, HQCity, HQStreet, Period, previousClose, open, marketCap);
+                }
+            }
+        }
+
+        void FillData(string csvDataP, string csvDataS, string csvDataHistory)
+        {
+                                   
             string csvLine;
             int index;
             using (StringReader reader = new StringReader(csvDataP))
@@ -84,29 +132,26 @@ namespace WebApp
                     int tempYear;
                     if (int.TryParse(getNumber(getBetween(csvLine, "founded", ",\"fullTimeEmployees")), out tempYear))
                     {
-                        newItem.YearFounded = tempYear;
+                        YearFounded = tempYear;
                     }
 
                     double tempEmpoyees;
                     if (Double.TryParse(getBetween(csvLine, "\"fullTimeEmployees\":", ",\"companyOfficers"), out tempEmpoyees))
                     {
-                        newItem.Employees = tempEmpoyees;
+                        Employees = tempEmpoyees;
                     }
 
-
-                    newItem.HQCity = getBetween(csvLine, "\"city\":", ",\"state").Replace("\"", "");
+                    HQCity = getBetween(csvLine, "\"city\":", ",\"state").Replace("\"", "");
 
                     if (csvLine.Contains("address2"))
                     {
-                        newItem.HQStreet = getBetween(csvLine, "\"address1\":", ",\"address2").Replace("\"", "");
+                        HQStreet = getBetween(csvLine, "\"address1\":", ",\"address2").Replace("\"", "");
                     }
                     else
                     {
-                        newItem.HQStreet = getBetween(csvLine, "\"address1\":", ",\"city").Replace("\"", "");
-                    }
-                                 
+                        HQStreet = getBetween(csvLine, "\"address1\":", ",\"city").Replace("\"", "");
+                    }     
                 }
-
             }
 
             using (StringReader reader = new StringReader(csvDataS))
@@ -114,39 +159,39 @@ namespace WebApp
                 while ((csvLine = reader.ReadLine()) != null)
                 {
                     string[] splitLine = csvLine.Split(',');
-
-                    index = Array.FindIndex(splitLine, row => row.Contains("previousClose"));
-                                       
-                    double tempPreviousClose;
-                    if (Double.TryParse(string.Format(splitLine[index].Substring(23).Replace(".", ","), "{0:#,##0.##}"), out tempPreviousClose))
-                    {
-                        newItem.previousClose = tempPreviousClose;
-                    }
-
-                    index = Array.FindIndex(splitLine, row => row.Contains("open"));
-                
-                    double tempOpen;
-                    if (Double.TryParse(string.Format(splitLine[index].Substring(14).Replace(".", ","), "{0:#,##0.##}"), out tempOpen))
-                    {
-                        newItem.open = tempOpen;
-                    }
+                                        
                     index = Array.FindIndex(splitLine, row => row.Contains("marketCap"));
               
                     double tempmarketCap;
-                    if (splitLine[index].Length > 23)
+                    if (index >=0 && splitLine[index].Length > 23)
                     {
-                        if (Double.TryParse(string.Format(splitLine[index].Substring(23).Replace(".", ","), "{0:#,##0.##}"), out tempmarketCap))
+                        if (Double.TryParse(splitLine[index].Substring(23).Replace(".", ","), out tempmarketCap))
                         {
-                            newItem.marketCap = tempmarketCap;
+                            marketCap = tempmarketCap;
                         }
-                    }
-                   
-                    parsedStockData.Add(DateTime.Now, newItem);
+                    }                 
                 }
-                    
             }
 
-            return parsedStockData;
+            using (StringReader reader = new StringReader(csvDataHistory))
+            {
+                while ((csvLine = reader.ReadLine()) != null)
+                {
+                    string[] splitLine = csvLine.Split(',');
+
+                    double tempOpen;
+                    if (Double.TryParse(splitLine[8].Replace(".", ","), out tempOpen))
+                    {
+                        open = tempOpen;
+                    }
+
+                    double tempPreviousClose;
+                    if (Double.TryParse(splitLine[11].Replace(".", ","), out tempPreviousClose))
+                    {
+                        previousClose = tempPreviousClose;
+                    }                
+                }
+            }
         }
 
         string getNumber(string a)
@@ -172,6 +217,60 @@ namespace WebApp
             }
 
             return "";
+        }
+
+        public void Insert_Data(string _Symbol, string _Company, int _Year, Double _Employees, string _City, string _Street, DateTime _Period, Double _PCP, Double _OP, Double _MC)
+        {
+
+            String query = "INSERT INTO [dbo].[YahooTable] ([Symbol], [CompanyName], [Year], [Employees], [HqCity], [HqStreet], [Date], [PCP], [OP], [MC]) VALUES (@Symbol, @CompanyName, @Year, @Employees, @HqCity, @HqStreet, @Date, @PCP, @OP, @MC)";
+
+            cnn = new SqlConnection(con);
+            cnn.Open();
+            cmd = new SqlCommand(query, cnn);
+
+            cmd.Parameters.AddWithValue("@Symbol", _Symbol);
+            cmd.Parameters.AddWithValue("@CompanyName", _Company);
+            cmd.Parameters.AddWithValue("@Year", _Year);
+            cmd.Parameters.AddWithValue("@Employees", _Employees);
+            cmd.Parameters.AddWithValue("@HqCity", _City);
+            cmd.Parameters.AddWithValue("@HqStreet", _Street);
+            cmd.Parameters.AddWithValue("@Date", _Period);
+            cmd.Parameters.AddWithValue("@PCP", _PCP);
+            cmd.Parameters.AddWithValue("@OP", _OP);
+            cmd.Parameters.AddWithValue("@MC", _MC);
+            cmd.ExecuteNonQuery();
+            cnn.Close();
+
+        }
+
+        public void Delete_Data()
+        {
+            cnn = new SqlConnection(con);
+            cnn.Open();
+            cmd = new SqlCommand("Truncate table YahooTable", cnn);
+            cmd.ExecuteNonQuery();
+            cnn.Close();
+        }
+
+        void GetFullName(string symbol)
+        {                   
+            string htmlLink = string.Format("https://finance.yahoo.com/quote/{0}/profile?p={0}", symbol);
+            var getHtmlWeb = new HtmlAgilityPack.HtmlWeb();
+            var document = getHtmlWeb.Load(htmlLink);
+            var aTags = document.DocumentNode.SelectNodes("//h1[contains(@class, 'D(ib) Fz(18px)')]");
+            if (aTags != null)
+            {
+                foreach (var aTag in aTags)
+                {
+                    string d = aTag.InnerHtml;
+                    CompanyName = HttpUtility.HtmlDecode(d.ToString());
+                }
+            }
+            else
+            {
+                CompanyName = "";
+            }
+        
         }
     }
 }
